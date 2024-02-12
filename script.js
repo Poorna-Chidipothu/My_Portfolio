0e04ed180891b6c5255e6e99da756b9bc1dec48a
CH POORNA CHANDRA <111335589+Poorna55@users.noreply.github.com>
2024-02-12 11:03:42 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const UpArrow = document.querySelector('.up');
 
-window.addEventListener('scroll',()=>{
-    if(window.scrollY > 500){
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
         UpArrow.style.opacity = '1';
         UpArrow.style.pointerEvents = 'all';
-    }else{
+    } else {
         UpArrow.style.opacity = '0';
         UpArrow.style.pointerEvents = 'none';
     }
@@ -13,55 +13,55 @@ window.addEventListener('scroll',()=>{
 
 
 const lightDark = document.querySelector('.dl_mode');
-lightDark.addEventListener('click',()=>{
+lightDark.addEventListener('click', () => {
     let body = document.body;
     let dl_icon = document.querySelector('.dl');
     body.classList.toggle("light_mode");
-    if (body.classList.contains("light_mode")){
+    if (body.classList.contains("light_mode")) {
         dl_icon.name = 'moon';
     }
-    else{
+    else {
         dl_icon.name = 'sunny';
     }
 })
 
-// document.addEventListener('contextmenu', event => {
-//     event.preventDefault();
-// });
+document.addEventListener('contextmenu', event => {
+    event.preventDefault();
+});
 const successicn = document.querySelector('.icn');
 const successmsg = document.querySelector('.success_msg');
 const conForm = document.getElementById('form_container');
-        conForm.addEventListener('submit', function(event) {
-            event.preventDefault();
-            console.log("Button clicked!");
-            // EmailJS Send Function
-            emailjs.send('service_q7avzw9','template_qe5bx8p',{
-                user_name: document.querySelector('#user_name').value,
-                user_email: document.querySelector('#user_email').value,
-                message: document.querySelector('#message').value
-                },'Fba-CgXAgYaLVZ7we')
-                .then((result) => {
-                    /* If you want to see the response in an alert then uncomment below lines */
-                    // alert("Sent!")
-                    successicn.name = 'checkmark-circle';
-                    successmsg.innerHTML = 'Sent Successfully...';
-                    successicn.style.display = 'block';
-                    successmsg.style.display = 'block';
-                    setTimeout(()=>{
-                        successicn.style.display = 'none';
-                        successmsg.style.display = 'none';
-                    },3000);
-                    conForm.reset();
-                }, (error) => {
-                    // alert("Error Occured while Sending your Message.")
-                    successicn.name = 'close-circle';
-                    successmsg.innerHTML = 'Failed to Send Message...';
-                    successicn.style.display = 'block';
-                    successmsg.style.display = 'block';
-                    setTimeout(()=>{
-                        successicn.style.display = 'none';
-                        successmsg.style.display = 'none';
-                    },3000);
-                    console.log(error.toString());
-                });
+conForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // console.log("Button clicked!");
+    // EmailJS Send Function
+    emailjs.send('service_q7avzw9', 'template_qe5bx8p', {
+        user_name: document.querySelector('#user_name').value,
+        user_email: document.querySelector('#user_email').value,
+        message: document.querySelector('#message').value
+    }, 'Fba-CgXAgYaLVZ7we')
+        .then((result) => {
+            /* If you want to see the response in an alert then uncomment below lines */
+            // alert("Sent!")
+            successicn.name = 'checkmark-circle';
+            successmsg.innerHTML = 'Sent Successfully...';
+            successicn.style.display = 'block';
+            successmsg.style.display = 'block';
+            setTimeout(() => {
+                successicn.style.display = 'none';
+                successmsg.style.display = 'none';
+            }, 3000);
+            conForm.reset();
+        }, (error) => {
+            // alert("Error Occured while Sending your Message.")
+            successicn.name = 'close-circle';
+            successmsg.innerHTML = 'Failed to Send Message...';
+            successicn.style.display = 'block';
+            successmsg.style.display = 'block';
+            setTimeout(() => {
+                successicn.style.display = 'none';
+                successmsg.style.display = 'none';
+            }, 3000);
+            console.log(error.toString());
         });
+});
